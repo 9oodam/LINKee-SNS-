46b0e5f09d69fd5bdc41384086cb84432bc14596
9oodam <godamcity98@gmail.com>
2023-06-08 21:00:14 +0900

    main render 완성

--- backEnd/controllers/mainCon.js
+++ backEnd/controllers/mainCon.js
@@ -1,30 +1,28 @@
 const {User, Post, LikePost} = require("../models");
 
 exports.getPost = async (req, res) => {
-    const {user_id} = req.acc_decoded;
+    const {id} = req.acc_decoded;
     try {
         // 로그인 된 유저
-        const user = await User.findOne({where : {user_id}});
-        console.log(user);
+        const user = await User.findOne({where : {id}});
+
+        // 모든 유저
+        const userAll = await User.findAll();
 
         // 로그인 된 유저가 팔로우 하고 있는 유저들
         const following = JSON.parse(user.following);
-        console.log(following); // 배열로 받아와 지는지 확인 [2, 3]
 
         // 팔로우 하고 있는 유저들의 게시글
         const followingPost = [];
 
         // for of : 순차적
         // forEach : 비순차적
-        for (const el of following) {
+        for await (const el of following) {
             const temp = await Post.findAll({ where: { user_id: el } });
-            // const tempArray=temp.map((a)=>{
-            // return a.dataValues;
-            // })
-            followingPost.push(temp);
+            followingPost.push(...temp);
         }
 
-        res.json({user, following, followingPost});
+        res.json({user, userAll, following, followingPost});
         
     } catch (error) {
         console.log(error);
@@ -33,7 +31,6 @@ exports.getPost = async (req, res) => {
 
 
 exports.getProfile = async (req, res) => {
-    console.log(req.acc_decoded);
     const {id} = req.acc_decoded;
 
     try {
--- backEnd/routers/mainRouter.js
+++ backEnd/routers/mainRouter.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const {isLogin} = require("../middleware/isLogin");
 // levelCheck 가져오기
 
-const {sendFollowingUserPost, getPost, getProfile} = require("../controllers/mainCon");
+const {getPost, getProfile} = require("../controllers/mainCon");
 
-router.get('/', isLogin,getPost );
+router.get('/', isLogin, getPost);
 
 router.get('/getProfile', isLogin, getProfile);
 
