8c65bfe2c7e307d6976d22ceafe4ead1a2a73457
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-12 13:36:40 +0900

    css link fixing

--- backEnd/app.js
+++ backEnd/app.js
@@ -45,6 +45,7 @@ sequelize.sync({force : false}).then((e) => {
 // 정적 폴더 경로
 app.use("/post_img", express.static(path.join(__dirname, "post_img")));
 app.use("/user_img", express.static(path.join(__dirname, "user_img")));
+app.use(express.static("/home/ubuntu/frontEnd/css"));
 
 // router 연결
 app.use("/signup", signupRouter);
