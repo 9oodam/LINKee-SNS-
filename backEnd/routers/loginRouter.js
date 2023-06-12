db0103df0ff0e5b025706dadcae9eac90923d109
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-12 11:59:58 +0900

    view path setting editing

--- backEnd/routers/loginRouter.js
+++ backEnd/routers/loginRouter.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const {signUp1, login1, idCheck, nicknameCheck, loginalert, passwordalert, admin1, admin2, levelchange, deny, dayCnt, unAuth} = require("../controllers/userCon");
 
+const path = require("path");
+router.get("/",(req,res)=>{
+    const filePath = path.join(__diname,"..","frontend","page","login.html");
+});
+
 router.get("/loginalert/:id", loginalert);
 router.get("/passwordalert", passwordalert);
 
