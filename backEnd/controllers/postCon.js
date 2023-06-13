295f8765d85c90a24c57af0a9ffe795a2725cb71
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-13 13:54:12 +0900

    backend connection error fix

--- backEnd/controllers/postCon.js
+++ backEnd/controllers/postCon.js
@@ -1,14 +1,17 @@
 const {Post} = require("../models");
 
-exports.myPost = async (req,res,next)=>{
+exports.myPost = async (req,res)=>{
     const {acc_decoded} = req;
     const writer = acc_decoded.username;
     Post.findAll({
         where : {by : writer}
     }).then(posts=>{
         const post = posts.map(user => user.get({plain : true}));
-        res.json(post);
-        next();
+        const response = {
+            data : post,
+            html : "/home/ubuntu/frontEnd/page/mypage.html"
+        };
+        res.json(response);
     }).catch((error)=>{
         console.log(error);
         console.log("unable to show the table in controller");
