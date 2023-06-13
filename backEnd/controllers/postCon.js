cf8f6290721bbebf2ccafec7893afe0c13378401
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-13 13:47:25 +0900

    backend connection error fixing

--- backEnd/controllers/postCon.js
+++ backEnd/controllers/postCon.js
@@ -1,6 +1,6 @@
 const {Post} = require("../models");
 
-exports.myPost = async (req,res)=>{
+exports.myPost = async (req,res,next)=>{
     const {acc_decoded} = req;
     const writer = acc_decoded.username;
     Post.findAll({
@@ -8,6 +8,7 @@ exports.myPost = async (req,res)=>{
     }).then(posts=>{
         const post = posts.map(user => user.get({plain : true}));
         res.json(post);
+        next();
     }).catch((error)=>{
         console.log(error);
         console.log("unable to show the table in controller");
--- backEnd/routers/mypageRouter.js
+++ backEnd/routers/mypageRouter.js
@@ -5,12 +5,14 @@ const {myPost,mypage,editProfile} = require("../controllers/mypageCon");
 const {uploadImg,updateProfileImg} = require("../middleware/uploadImg");
 
 const path = require("path");
-router.get("/",(req,res)=>{
-    const filePath = "/home/ubuntu/frontEnd/page/mypage.html";
-    res.sendFile(filePath);
-});
+// router.get("/",(req,res)=>{
+//     const filePath = "/home/ubuntu/frontEnd/page/mypage.html";
+//     res.sendFile(filePath);
+// });
 
-router.get("/",isLogin,myPost);
+router.get("/",isLogin,myPost,(req,res)=>{
+    res.sendFile("/home/ubuntu/frontEnd/page/mypage.html");
+});
 
 router.get("/users",isLogin,mypage);
 
