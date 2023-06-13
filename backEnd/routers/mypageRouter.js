c48561516dd1e5aa8af66ccf94fd52de1e7c4c8f
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-13 17:15:20 +0900

    backend connection fix

--- backEnd/routers/mypageRouter.js
+++ backEnd/routers/mypageRouter.js
@@ -5,13 +5,16 @@ const {myPost,mypage,editProfile,currentUser} = require("../controllers/mypageCo
 const {uploadImg,updateProfileImg} = require("../middleware/uploadImg");
 
 const path = require("path");
-router.get("/",isLogin,currentUser);
+router.get("/",isLogin, async (req,res)=>{
+    const data = await currentUser(req,res);
+    console.log(data);
+});
 
 router.get("/mypost",isLogin,myPost);
 
 router.get("/users",isLogin,mypage);
 
-router.get("/edit",(req,res)=>{
+router.get("/edit",isLogin,(req,res)=>{
     const filePath = "/home/ubuntu/frontEnd/page/editProfile.html";
     res.sendFile(filePath);
 });
