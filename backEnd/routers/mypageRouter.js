79a70152a7776f806e2a13ff0302f4039bc9839d
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-14 09:59:18 +0900

    mypage edit ing

--- backEnd/routers/mypageRouter.js
+++ backEnd/routers/mypageRouter.js
@@ -9,14 +9,12 @@ router.get("/",isLogin, async (req,res)=>{
     const {acc_decoded} = req;
     const {user_id} = acc_decoded;
     console.log(user_id);
-    res.json(acc_decoded);
+    res.redirect(`/${user_id}`);
 });
 
 router.get("/mypost",isLogin,myPost);
 
-router.get("/users",isLogin,mypage,(req,res)=>{
-    console.log(req);
-});
+router.get("/users",isLogin,mypage);
 
 router.get("/edit",isLogin,(req,res)=>{
     const filePath = "/home/ubuntu/frontEnd/page/editProfile.html";
