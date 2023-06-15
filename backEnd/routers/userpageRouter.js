5ddca0c0df1846eb0f00d94ff611a3474886077f
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-15 18:01:02 +0900

    userpage connection error

--- backEnd/routers/userpageRouter.js
+++ backEnd/routers/userpageRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {isLogin} = require("../middleware/isLogin");
 
-router.get("/",isLogin,(req,res)=>{
+router.get("/:id",isLogin,(req,res)=>{
     res.sendFile("/home/ubuntu/frontEnd/page/search.html");
 });
 
