6fb630f5a03eabc23a9751c951a9b7916b25b8bd
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-19 13:54:36 +0900

    socket error fixing

--- backEnd/app.js
+++ backEnd/app.js
@@ -151,7 +151,7 @@ const server = app.listen(8080, () => {
 
 const io = socketIo(server,{
     cors : {
-        origin : "andybyungjoopark.com",
+        origin : "https://andybyungjoopark.com",
         credentials : true
     }
 });
