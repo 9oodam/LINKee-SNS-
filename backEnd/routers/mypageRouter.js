1cdd09bd49bb2e85155ab1d31358a7b3f003acda
9oodam <godamcity98@gmail.com>
2023-06-19 17:33:58 +0900

    mypage 수정

--- backEnd/routers/mypageRouter.js
+++ backEnd/routers/mypageRouter.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const {isLogin} = require("../middleware/isLogin");
 const {myPost} = require("../controllers/mypageCon");
 
-router.get("/",isLogin,myPost);
+router.get("/mypost",isLogin,myPost);
 
 module.exports = router;
