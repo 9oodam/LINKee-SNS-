4a2a7cd6a4db347f8cd4a9b5a7ae26ffc9457e2f
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-19 17:32:26 +0900

    socket connection fixing

--- frontEnd/js/notiSocket.js
+++ frontEnd/js/notiSocket.js
@@ -1,4 +1,4 @@
-const socket = io.connect('andybyungjoopark.com');
+const socket = io.connect('https://andybyungjoopark.com');
 let liveChatNoti = document.querySelector("#live_chat_noti_wrap");
 
 // 대화 요청 보내기
