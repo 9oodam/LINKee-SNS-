fa1bf6d7b9b836a09e7cd6784705835f7f7dd853
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-20 16:53:13 +0900

    detail page rendering

--- backEnd/routers/detailRouter.js
+++ backEnd/routers/detailRouter.js
@@ -4,6 +4,10 @@ const {isLogin} = require("../middleware/isLogin");
 const {getProfile, getcontents, delBtn, getlike, likeClick, bigComment, smallComment1, c_comment_nick1, likebigcomment1, getbiglike1, likesmallComment1} = require("../controllers/mainCon");
 const { SmallComment } = require("../models");
 
+router.get("/",isLogin,(req,res)=>{
+    res.sendFile("/home/ubuntu/frontEnd/page/detail.html");
+});
+
 router.get('/getProfile', isLogin, getProfile);
 
 router.get('/getcontents/:id', isLogin, getcontents);
