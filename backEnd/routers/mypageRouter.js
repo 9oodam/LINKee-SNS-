91cfa8ae9f10bc516138a84d2cf32e420d111754
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-20 15:34:33 +0900

    profile edit page image size error

--- backEnd/routers/mypageRouter.js
+++ backEnd/routers/mypageRouter.js
@@ -18,7 +18,11 @@ router.get("/edit",isLogin,(req,res)=>{
     res.sendFile("/home/ubuntu/frontEnd/page/editProfile.html");
 });
 
-router.post("/edit",isLogin, updateProfileImg.single("img"), editProfile);
+router.post("/edit",isLogin, updateProfileImg.single("img"),(err,req,res,next)=>{
+    if(err instanceof multer.MulterError){
+        res.status(400).json({error : "파일이 너무 크네요"});
+    }
+} ,editProfile);
 
 // router.post("/editPic",isLogin,updateProfileImg.single("img"),(req,res)=>{
 //     console.log(req);
