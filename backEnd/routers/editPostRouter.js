f79b3062c76c9daa1d27c60c55dac3d5046a50fa
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-21 15:54:14 +0900

    detail to editPost link fix ing

--- backEnd/routers/editPostRouter.js
+++ backEnd/routers/editPostRouter.js
@@ -4,3 +4,5 @@ const {isLogin} = require("../middleware/isLogin");
 router.get("/",isLogin,(req,res)=>{
     res.sendFile("/home/ubuntu/frontEnd/page/editPost.html");
 });
+
+module.exports = router;
