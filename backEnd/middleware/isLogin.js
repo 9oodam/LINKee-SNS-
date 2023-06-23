6e06828b42239011be71ac76dc46c3a60e86c285
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-23 15:54:31 +0900

    token expire to login page

--- backEnd/middleware/isLogin.js
+++ backEnd/middleware/isLogin.js
@@ -6,7 +6,8 @@ exports.isLogin = (req,res,next) =>{
 
     jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, (err, acc_decoded)=>{
         if(err){
-            next();
+            const filePath = "/home/ubuntu/frontEnd/page/login.html";
+            res.sendFile(filePath);
         }else{
             req.acc_decoded = acc_decoded;
             // 다음 미들웨어 실행
--- backEnd/routers/loginRouter.js
+++ backEnd/routers/loginRouter.js
@@ -3,7 +3,7 @@ const {isLogin} = require("../middleware/isLogin");
 const {signUp1, login1, idCheck, nicknameCheck, loginalert, passwordalert, admin1, admin2, levelchange, deny, dayCnt, unAuth} = require("../controllers/userCon");
 
 const path = require("path");
-router.get("/",isLogin,(req,res)=>{
+router.get("/",(req,res)=>{
     const filePath = "/home/ubuntu/frontEnd/page/login.html";
     res.sendFile(filePath);
 });
