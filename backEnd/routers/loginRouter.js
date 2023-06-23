206064c9fa12ce1bf63c2a83212f9c5adcab54ad
byungjoo0806 <andybyungjoopark@gmail.com>
2023-06-23 15:49:30 +0900

    token expire to login page

--- backEnd/routers/loginRouter.js
+++ backEnd/routers/loginRouter.js
@@ -3,7 +3,7 @@ const {isLogin} = require("../middleware/isLogin");
 const {signUp1, login1, idCheck, nicknameCheck, loginalert, passwordalert, admin1, admin2, levelchange, deny, dayCnt, unAuth} = require("../controllers/userCon");
 
 const path = require("path");
-router.get("/",(req,res)=>{
+router.get("/",isLogin,(req,res)=>{
     const filePath = "/home/ubuntu/frontEnd/page/login.html";
     res.sendFile(filePath);
 });
